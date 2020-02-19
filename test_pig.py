34de012775b88f7ed2b40c577b03108429804966
Brian Bonsignore <bonsignore.brian@gmail.com>
2020-02-19 14:31:13 -0500

    Adds tests for Game.player instances

--- test_pig.py
+++ test_pig.py
@@ -5,28 +5,28 @@ VALUES = [i for i in range(1, 7)]
 
 
 def test_create_game():
-    game = Game()
-    assert game is not None
+    game = Game(6)
+    assert isinstance(game.player1, Player)
+    assert isinstance(game.player2, Player)
 
 
 def test_create_player():
     player = Player('Human')
-    assert player is not None
     assert player.name == 'Human'
     assert player.score == 0
     assert player.hold == 0
+    assert player.action == 'roll'
 
 
 def test_create_die():
-    die = Die()
-    assert die is not None
+    die = Die(6)
     assert die.values == VALUES
     assert str(die) == "6-sided die"
 
 
 def test_roll_die():
-    die = Die()
-    rolls = {die.roll() for i in range(1000)}
+    die = Die(6)
+    rolls = {die.roll() for i in range(10000)}
     assert rolls == set(VALUES)
     for i in range(50):
         assert die.roll() in VALUES
@@ -34,4 +34,4 @@ def test_roll_die():
 
 def test_player_roll():
     player = Player('Human')
-    player.roll(Die())
+    player.roll(Die(6))
