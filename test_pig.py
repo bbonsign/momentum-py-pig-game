867a494d41c5f524ab90adc6a5aa560552d73fc5
Brian Bonsignore <bonsignore.brian@gmail.com>
2020-02-19 13:21:55 -0500

    Adds tests for attributes and rolling random die

--- test_pig.py
+++ test_pig.py
@@ -1,16 +1,30 @@
-import pytest
+# import pytest
 from pig import Game, Player, Die
+
+VALUES = [i for i in range(1, 7)]
 
 
 def test_create_player():
     player = Player('Human')
-    assert player != None
-    assert player.name = 'Human'
+    assert player is not None
+    assert player.name == 'Human'
+    assert player.score == 0
+
 
 def test_create_game():
     game = Game()
-    assert game != None
+    assert game is not None
+
 
 def test_create_die():
     die = Die()
-    assert die != None
+    assert die is not None
+    assert die.values == VALUES
+
+
+def test_roll_die():
+    die = Die()
+    rolls = {die.roll() for i in range(1000)}
+    assert rolls == set(VALUES)
+    for i in range(50):
+        assert die.roll() in VALUES
